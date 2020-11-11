2f7e3ab2545000758459d261e7052b5dc524b4fd
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-11-10 17:45:58 -0800

    initial push

---
+++ __tests__/app.test.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+
+const { execSync } = require('child_process');
+
+const fakeRequest = require('supertest');
+const app = require('../lib/app');
+const client = require('../lib/client');
+
+describe('app routes', () => {
+  describe('routes', () => {
+    let token;
+  
+    beforeAll(async done => {
+      execSync('npm run setup-db');
+  
+      client.connect();
+  
+      const signInData = await fakeRequest(app)
+        .post('/auth/signup')
+        .send({
+          email: 'jon@user.com',
+          password: '1234'
+        });
+      
+      token = signInData.body.token;
+  
+      return done();
+    });
+  
+    afterAll(done => {
+      return client.end(done);
+    });
+
+    test('returns todos', async() => {
+
+      const expectation = [
+        {
+          id: 1,
+          todo: 'todos los mujer es muy perezoso',
+          completed: false,
+        },
+        {
+          id: 2,
+          todo: 'todo el mundo es loco',
+          completed: false,
+        },
+        {
+          id: 3,
+          todo: 'todos como se donde',
+          completed: false, 
+        }
+      ];
+      
+
+      const data = await fakeRequest(app)
+        .get('/todos')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+  });
+});
---
+++ data/todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    todo: 'todos los mujer es muy perezoso',
+    completed: false,
+  },
+  {
+    id: 2,
+    todo: 'todo el mundo es loco',
+    completed: false,
+  },
+  {
+    id: 3,
+    todo: 'todos como se donde',
+    completed: false, 
+  }
+];
